d35459ba494001bd540e0013d8643e16e308a704
=Wellington Nascimento <=wellingtonwsn2015@gmail.com>
2025-03-17 19:22:30 -0300

    Configurando Spring x Tomcat

---
+++ src/main/java/com/ninjasNa/gerenciamentoNinjas/Controller.java
@@ -0,0 +1,16 @@
+package com.ninjasNa.gerenciamentoNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boavindas")
+    public String boaVindas(){
+        return  "Essa é minha primeira mensagem";
+    }
+
+}
--- src/main/java/com/ninjasNa/gerenciamentoNinjas/GerenciamentoNinjasApplication.java
+++ src/main/java/com/ninjasNa/gerenciamentoNinjas/GerenciamentoNinjasApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GerenciamentoNinjasApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GerenciamentoNinjasApplication.class, args);
 	}
 
