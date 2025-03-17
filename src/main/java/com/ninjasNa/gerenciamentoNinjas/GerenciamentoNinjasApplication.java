5c1c79df30fb615bd98b51433bee7a15961ed147
=Wellington Nascimento <=wellingtonwsn2015@gmail.com>
2025-03-17 19:00:34 -0300

    Inicial

---
+++ src/main/java/com/ninjasNa/gerenciamentoNinjas/GerenciamentoNinjasApplication.java
@@ -0,0 +1,13 @@
+package com.ninjasNa.gerenciamentoNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ninjasNa/gerenciamentoNinjas/GerenciamentoNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninjasNa.gerenciamentoNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
